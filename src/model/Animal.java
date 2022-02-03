12d0b3b834c45ff237a5c0f94c792ba2950ae6a6
affenzellerkarin <91280638+affenzellerkarin@users.noreply.github.com>
2022-02-04 00:39:50 +0100

    Add files via upload
    Done

--- src/model/Animal.java
+++ src/model/Animal.java
@@ -1,4 +1,42 @@
 package model;
 
-public class Animal {
+public class Animal implements Comparable <Animal> {
+
+    private int animalID;
+    private String animal;
+    private int age;
+    private String geraeusch;
+
+    public Animal(int animalID, String animal, int age, String geraeusch) {
+        this.animalID = animalID;
+        this.animal = animal;
+        this.age = age;
+        this.geraeusch = geraeusch;
+    }
+
+
+
+    @Override
+    public int compareTo(Animal other) {
+        if (animalID == other.animalID) {
+            if (animal.equals(other.animal)) {
+                if (age == other.age) {
+                    return geraeusch.compareTo(other.geraeusch);
+                }
+                return this.age - other.age;
+
+            }
+            return animal.compareTo(other.animal);
+        }
+
+
+        return this.animalID - other.animalID;
+    }
+
+
+    @Override
+    public String toString() {
+        return (animalID + ", " + animal  + ", Alter: " + age + ", Geräusch: " + geraeusch + '\n');
+    }
+
 }
